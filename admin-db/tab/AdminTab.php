65aac28febf57199b6be7896960f9367f1c7e4fd
Sergey Kotov <kssp@list.ru>
2024-03-02 08:57:22 +0300

    Recreate database restriction

--- admin-db/tab/AdminTab.php
+++ admin-db/tab/AdminTab.php
@@ -5,7 +5,7 @@ class AdminTab {
     function __construct() {
         //parent::__construct();
         
-        global $app, $app_uri;
+        global $app, $app_uri, $app_dir;
         
 		Atk4\Ui\Button::addTo($app, [
 			'Download database initialization script'
@@ -14,51 +14,53 @@ class AdminTab {
 		->addClass('big blue button')
 		->on('click', $app->jsRedirect($app_uri . 'tool/create.sql', false));
 
-		Atk4\Ui\Button::addTo($app, [
-			'Recreate database structure'
-			, 'icon' => 'snowplow'
-		])
-		->addClass('big red button')
-		->on(
-			'click',
-			new Atk4\Ui\Js\JsModal('Creating database structure'
-				, $app->add([Atk4\Ui\VirtualPage::class])
-				->set(
-					function (Atk4\Ui\VirtualPage $vp) {
-						Atk4\Ui\Loader::addTo($vp, ['shim' => [\Atk4\Ui\Message::class, 'Erasing all the data...', 'class.red' => true]])
-						->set(
-							function (\Atk4\Ui\Loader $p) {
-								global $app, $app_dir;
-								$code = 0;
-								$output = [];
-								
-								try {
-									$script = file_get_contents($app_dir . 'tool/create.sql');
-									if ($script) {
-										$app->db->getConnection()->getConnection()->exec($script);
-										array_push($output, 'Database is empty now');
-									} else {
-										throw new Exception('SQL script reading error');
+		if (file_exists($app_dir . '../composer.phar')) {
+			Atk4\Ui\Button::addTo($app, [
+				'Recreate database structure'
+				, 'icon' => 'snowplow'
+			])
+			->addClass('big red button')
+			->on(
+				'click',
+				new Atk4\Ui\Js\JsModal('Creating database structure'
+					, $app->add([Atk4\Ui\VirtualPage::class])
+					->set(
+						function (Atk4\Ui\VirtualPage $vp) {
+							Atk4\Ui\Loader::addTo($vp, ['shim' => [\Atk4\Ui\Message::class, 'Erasing all the data...', 'class.red' => true]])
+							->set(
+								function (\Atk4\Ui\Loader $p) {
+									global $app, $app_dir;
+									$code = 0;
+									$output = [];
+									
+									try {
+										$script = file_get_contents($app_dir . 'tool/create.sql');
+										if ($script) {
+											$app->db->getConnection()->getConnection()->exec($script);
+											array_push($output, 'Database is empty now');
+										} else {
+											throw new Exception('SQL script reading error');
+										}
+									} catch (Exception $e) {
+										array_push($output, $e->getMessage());
+										$code = -1;
 									}
-								} catch (Exception $e) {
-									array_push($output, $e->getMessage());
-									$code = -1;
+									
+									$this->addLoaderMessage($p
+										, $code == 0
+											? 'Database recreated succesfully'
+											: 'Error occured - database is probably ruined'
+										, $output
+										, $code
+									);
+									$this->addLoaderRefreshButton($p);
 								}
-								
-								$this->addLoaderMessage($p
-									, $code == 0
-										? 'Database recreated succesfully'
-										: 'Error occured - database is probably ruined'
-									, $output
-									, $code
-								);
-								$this->addLoaderRefreshButton($p);
-							}
-						);
-					}
+							);
+						}
+					)
 				)
-			)
-		);
+			);
+		}
 
 		Atk4\Ui\View::addTo($app, ['ui' => 'hidden divider']);
 
