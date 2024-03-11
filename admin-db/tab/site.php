830e764dfef2af632b9f81570cf45a8e9e4a2c0a
Sergey Kotov <kssp@list.ru>
2024-03-11 21:20:10 +0300

    Database upgrading while site update

--- admin-db/tab/site.php
+++ admin-db/tab/site.php
@@ -20,6 +20,25 @@ Atk4\Ui\Button::addTo($app, [
 			$code = 0;
 			$res = exec('php "' . $app_dir . 'tool/update.php" "' . getenv('ADMIN_DB_UPDATE_URL') . '"', $output, $code);
 			
+			if ($code == 0) {
+				array_push($output, 'Database upgrading...');
+				
+				try {
+					$script = file_get_contents($app_dir . 'tool/create.sql');
+					
+					if ($script) {
+						$app->db->getConnection()->getConnection()->exec($script);
+					} else {
+						throw new Exception('SQL script reading error');
+					}
+					
+					array_push($output, 'Database is up to date now');
+				} catch (Exception $e) {
+					array_push($output, $e->getMessage());
+					$code = -1;
+				}
+			}
+			
 			$p->addMessage($code == 0
 					? 'You are lucky - the site is up to date now'
 					: 'Error occured - the site is probably ruined'
